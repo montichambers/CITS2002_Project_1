9e98b6e23330d32ef1439a5d329c717d6feec271
Nathan Nguyen <nathan0761@gmail.com>
2022-08-30 16:42:48 +0800

    Project 1

---
+++ cmake-build-debug/CMakeFiles/Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_1.dir/estimatecron.c.o"
+  "CMakeFiles/Project_1.dir/estimatecron.c.o.d"
+  "Project_1"
+  "Project_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
